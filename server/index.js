6f79fcbf16db492268582ef4192c85195dc32055
Vikas Pritam <pritam.developer.1@gmail.com>
2025-01-26 17:45:52 +0530

    added mongoose

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const port = 8888;
 
 const app = express();
@@ -12,4 +14,11 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
+});
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=> {
+    console.log('connected to db');
+}).catch((error) =>{
+     console.log('Error connecting: ', error);
 });
