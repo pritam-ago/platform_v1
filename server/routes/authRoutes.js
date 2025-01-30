25c35cf1afbe5d492abbbecfd01b611bd686acd3
Vikas Pritam <pritam.developer.1@gmail.com>
2025-01-30 17:21:00 +0530

    debugged some shii

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { signup, login, getUserProfile } from "../controllers/authController.js";
-import authMiddleware from "../middleware/authMiddleware.js";
+import authMiddleware from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
